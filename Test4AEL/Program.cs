b8bf56734ae1ff2c6916a8ad1b344d9d19989b06
hannesa7x <bruhan@rgtfo-me.it>
2020-04-16 10:33:14 +0200

    Programmbeschreibung hinzugefügt

--- Test4AEL/Program.cs
+++ Test4AEL/Program.cs
@@ -2,10 +2,12 @@
 
 namespace Test4AEL
 {
+
+    //Konvertierung von Zahlensystemen
     class Program
     {
         static void Main(string[] args)
-        {
+        {           
             Console.WriteLine("Hello World!");
             Console.ReadLine();
         }
