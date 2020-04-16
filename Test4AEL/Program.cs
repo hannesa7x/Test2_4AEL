5bbf84f8e6b7bb5e69bc36c5bdab9b0607568cb5
hannesa7x <bruhan@rgtfo-me.it>
2020-04-16 10:15:02 +0200

    1. commit

---
+++ Test4AEL/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Test4AEL
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
